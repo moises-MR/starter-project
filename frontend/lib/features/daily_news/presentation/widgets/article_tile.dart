4873a2d2c45b3f84837a6365f8fd56c4b8a6d38c
Moises Ramirez <moisesabraham0906@gmail.com>
2026-04-24 10:46:04 -0600

    refactor: remove unused imports from article_tile.dart

--- frontend/lib/features/daily_news/presentation/widgets/article_tile.dart
+++ frontend/lib/features/daily_news/presentation/widgets/article_tile.dart
@@ -1,5 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app_clean_architecture/core/utils/date_formatter.dart';
 
